7e269411bceabce0f3d1338acfce847bd264aa8a
ssangwoos <8_beauty@naver.com>
2026-01-05 00:02:17 +0900

    Update firebase-config.js

--- ledger-8pmd1st/firebase-config.js
+++ ledger-8pmd1st/firebase-config.js
@@ -1,11 +1,11 @@
 // firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyD6gYbjdXjdwLNP4baJTxKZpqbaSFkG670",
-  authDomain: "ledger-cmr8p.firebaseapp.com",
-  projectId: "ledger-cmr8p",
-  storageBucket: "ledger-cmr8p.firebasestorage.app",
-  messagingSenderId: "556126872516",
-  appId: "1:556126872516:web:2c5e1044619d5201837a91"
+  apiKey: "AIzaSyDHENfqV16JMUBvatYvJq4F3FPvvyCWMqM",
+  authDomain: "ledger-8pmd1st.firebaseapp.com",
+  projectId: "ledger-8pmd1st",
+  storageBucket: "ledger-8pmd1st.firebasestorage.app",
+  messagingSenderId: "396567030990",
+  appId: "1:396567030990:web:5ae5265c49d7800fcbf49f"
 };
 
 // 초기화
