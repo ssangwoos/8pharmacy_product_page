fa616217d2c271d875f4ec14e3d3f2b2a7ab2118
ssangwoos <8_beauty@naver.com>
2026-08-26 16:24:41 +0900

    Update config.js

--- order/8phmd1/config.js
+++ order/8phmd1/config.js
@@ -21,3 +21,12 @@ export const firebaseConfig = {
     appId: "1:713414389922:web:606452de8b27fe847ca7fb"
 
 };
+
+export const LEDGER_CONFIG = {
+    apiKey: "AIzaSyDHENfqV16JMUBvatYvJq4F3FPvvyCWMqM",
+    authDomain: "ledger-8pmd1st.firebaseapp.com",
+    projectId: "ledger-8pmd1st",
+    storageBucket: "ledger-8pmd1st.firebasestorage.app",
+    messagingSenderId: "396567030990",
+    appId: "1:396567030990:web:5ae5265c49d7800fcbf49f"
+};
